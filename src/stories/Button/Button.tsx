e2a964d55ac6e3fdf193fdd8883bc49d6497428a
PeixuanSun <psun4@academic.rrc.ca>
2025-03-09 22:54:59 -0500

    2nd commit

--- src/stories/Button/Button.tsx
+++ src/stories/Button/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { ButtonProps } from './Button.types'
+import {     ButtonProps } from './Button.types'
 
 /** Styled button component */
 const StyledButton = styled.button<ButtonProps>`
